ab98d81282f7448bd6ae6944b8456c41f746b225
younes <reguiegyounes@gmail.com>
2021-02-03 20:35:57 +0100

    commit add NoContactScreen & ContactsScreen(ListView)

---
+++ client/lib/src/contactsScreen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:client/src/noContacts.dart';
+
+
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Contacts List',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ContactScreen(title: 'Contacts List'),
+    );
+  }
+}
+
+class ContactScreen extends StatefulWidget {
+  ContactScreen({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _ContactScreenState createState() => _ContactScreenState();
+}
+
+class _ContactScreenState extends State<ContactScreen> {
+  
+  List contacts=[];
+
+  void _addContact() {
+    setState(() {
+      contacts.add({'name':'smith'});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body:contacts.isEmpty? NoContact():ListView(
+        children:contacts.map<Widget>(
+          (contact) =>ListTile(
+            title: Text(contact['name']),
+          )).toList(),
+      ),
+      floatingActionButton:Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: (){},
+            tooltip: 'Refresh List',
+            child: Icon(Icons.refresh),
+            backgroundColor: Colors.purple
+          ),
+          SizedBox(width: 10,),
+          FloatingActionButton(
+            onPressed: _addContact,
+            tooltip: 'Add new contact',
+            child: Icon(Icons.person_add),
+          
+          ), 
+        ],
+      ) // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ client/lib/src/noContacts.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class NoContact extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.person_outline,
+              size: 80,
+              color: Colors.black45,
+            ),
+            Text(
+              'No contacts listed',
+            ),
+            FlatButton(
+              onPressed: (){},
+              child: Text(
+                'Add your first',
+                style: TextStyle(fontSize: 20,color: Colors.white),
+              ),
+              color: Colors.blue,
+            )
+          ],
+        ),
+      );
+  }
+}
